06bb9927cbcfc62a7d29938ac06048694b43e1ce
M-Desktop\Ryan <facebook0209@gmail.com>
2022-06-04 12:44:07 +0800

    re design ui

--- voice-ui/src/App.vue
+++ voice-ui/src/App.vue
@@ -4,13 +4,12 @@
 		<audio id="audio_off" src="mic_click_off.ogg"></audio>
 		<div v-if="voice.uiEnabled" class="voiceInfo">
 			<p v-if="voice.callInfo !== 0" :class="{ talking: voice.talking }">
-				[Call]
-			</p>
-			<p v-if="voice.radioEnabled && voice.radioChannel !== 0" :class="{ talking: voice.usingRadio }">
-				{{ voice.radioChannel }} Mhz [Radio]
-			</p>
-			<p v-if="voice.voiceModes.length" :class="{ talking: voice.talking }">
-				{{ voice.voiceModes[voice.voiceMode][1] }} [Range]
+				<button class="mdui-btn mdui-btn-icon mdui-color-theme-accent mdui-btn-raised" :class="{ talking: voice.talking }" :style="voice.voiceStatusStyle">
+					<i v-if="voice.callInfo !== 0" class="mdui-icon material-icons mdui-text-color-white-icon">phone_in_talk</i>
+					<i v-else-if="voice.radioEnabled && voice.radioChannel !== 0" class="mdui-icon material-icons mdui-text-color-white-icon">wifi_tethering</i>
+					<i v-else-if="voice.voiceModes.length" class="mdui-icon material-icons mdui-text-color-white-icon">mic</i>
+					<i v-else class="mdui-icon material-icons mdui-text-color-white-icon">mic_off</i>
+				</button>
 			</p>
 		</div>
 	</body>
@@ -50,6 +49,15 @@ export default {
 
 			if (data.voiceMode !== undefined) {
 				voice.voiceMode = data.voiceMode;
+				if (data.voiceMode == 0) {
+					voice.voiceStatusStyle = { background: "linear-gradient(180deg, #e6a400 0%, #916800 100%)", transition: "all 0.1s linear" }
+				} else if (data.voiceMode == 1) {
+					voice.voiceStatusStyle = { background: "linear-gradient(180deg, #18b70b 0%, #0d9900 100%)", transition: "all 0.1s linear" }
+				} else if (data.voiceMode == 2) {
+					voice.voiceStatusStyle = { background: "linear-gradient(180deg, #ff0000 0%, #920000 100%)", transition: "all 0.1s linear" }
+				} else {
+					voice.voiceStatusStyle = { background: "linear-gradient(180deg, #7e7e7e 0%, #424242 100%)", transition: "all 0.1s linear" }
+				}
 			}
 
 			if (data.radioChannel !== undefined) {
@@ -72,13 +80,13 @@ export default {
 				voice.talking = data.talking;
 			}
 
-			if (data.sound && voice.radioEnabled && voice.radioChannel !== 0) {
-				let click = document.getElementById(data.sound);
-				// discard these errors as its usually just a 'uncaught promise' from two clicks happening too fast.
-				click.load();
-				click.volume = data.volume;
-				click.play().catch((e) => {});
-			}
+			// if (data.sound && voice.radioEnabled && voice.radioChannel !== 0) {
+			// 	let click = document.getElementById(data.sound);
+			// 	// discard these errors as its usually just a 'uncaught promise' from two clicks happening too fast.
+			// 	click.load();
+			// 	click.volume = data.volume;
+			// 	click.play().catch((e) => {});
+			// }
 		});
 
 		fetch(`https://${GetParentResourceName()}/uiReady`, { method: 'POST' });
@@ -90,23 +98,60 @@ export default {
 
 <style>
 .voiceInfo {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	position: fixed;
-	text-align: right;
-	bottom: 5px;
-	padding: 0;
-	right: 5px;
-	font-size: 12px;
-	font-weight: bold;
-	color: rgb(148, 150, 151);
-	/* https://stackoverflow.com/questions/4772906/css-is-it-possible-to-add-a-black-outline-around-each-character-in-text */
-	text-shadow: 1.25px 0 0 #000, 0 -1.25px 0 #000, 0 1.25px 0 #000,
-		-1.25px 0 0 #000;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    position: fixed;
+    text-align: right;
+    padding: 0;
+    bottom: 8px;
+    left: 122px;
+    font-size: 12px;
+    font-weight: 700;
+    color: #949697;
+    text-shadow: 1.25px 0 0 #000, 0 -1.25px 0 #000, 0 1.25px 0 #000, -1.25px 0 0 #000
 }
+
+@-webkit-keyframes trew-soundwave {
+    0% {
+        box-shadow: 0 0 0 0 hsla(0, 0%, 100%, .75)
+    }
+
+    to {
+        box-shadow: 0 0 0 25px hsla(0, 0%, 100%, 0)
+    }
+}
+
+@keyframes trew-soundwave {
+    0% {
+        box-shadow: 0 0 0 0 hsla(0, 0%, 100%, .75)
+    }
+
+    to {
+        box-shadow: 0 0 0 25px hsla(0, 0%, 100%, 0)
+    }
+}
+
 .talking {
-	color: rgba(255, 255, 255, 0.822);
+    -webkit-animation: trew-soundwave 1s infinite;
+    animation: trew-soundwave 1s infinite;
+    border: 3px solid #00c711;
+    transition: all .1s linear
 }
+
+.mdui-btn-icon {
+    border-radius: 10px;
+    width: 34px;
+    height: 29px
+}
+
+.material-icons {
+    font-size: 18px
+}
+
 p {
-	margin: 0;
+    margin: 0
+}
+
+body {
+    background-color: transparent !important
 }
 </style>
