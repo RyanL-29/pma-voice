a8f10b618034eeec1171d117c487a5c33b7662c6
M-Desktop\Ryan <facebook0209@gmail.com>
2022-06-04 13:09:12 +0800

    hidden on pause Screen and fix the html script

--- client/utils/Nui.lua
+++ client/utils/Nui.lua
@@ -1,3 +1,4 @@
+local isUIToggle = true
 local uiReady = promise.new()
 function sendUIMessage(message)
 	Citizen.Await(uiReady)
@@ -12,5 +13,17 @@ end)
 
 RegisterNetEvent("voice_toggleui:toggle")
 AddEventHandler('voice_toggleui:toggle', function(toggle)
+	isUIToggle = toggle
 	sendUIMessage({uiEnabled = toggle})
+end)
+
+Citizen.CreateThread(function() 
+	while true do
+		Citizen.Wait(0)
+		if IsPauseMenuActive() then
+			sendUIMessage({uiEnabled = false})
+		elseif not IsPauseMenuActive() and isUIToggle then
+			sendUIMessage({uiEnabled = true})
+		end
+	end
 end)
